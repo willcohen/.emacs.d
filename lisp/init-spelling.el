362a1187fb72226e0e5ce09bb1dbaf785f9c3b3a
Will Cohen <willcohen@users.noreply.github.com>
2015-03-26 17:35:46 -0400

    Aspell for Windows

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -1,5 +1,7 @@
 (require 'ispell)
 
+(when *is-my-desktop* (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/"))
+
 (when (executable-find ispell-program-name)
   (require 'init-flyspell))
 
