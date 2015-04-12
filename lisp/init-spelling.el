550927cb7f13b0296efc16041a746d132a3e6e2e
Will Cohen <willcohen@users.noreply.github.com>
2015-04-12 01:12:48 -0400

    Version control and spelling

--- lisp/init-spelling.el
+++
@@ -1,8 +0,0 @@
-(require 'ispell)
-
-(when *is-my-desktop* (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/"))
-
-(when (executable-find ispell-program-name)
-  (require 'init-flyspell))
-
-(provide 'init-spelling)
