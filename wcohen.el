443d796eceb16b66772f9b7c03069dfca747b93e
Will Cohen <willcohen@users.noreply.github.com>
2015-07-30 12:18:57 -0400

    Magit completing read function change to builtin

--- wcohen.el
+++ wcohen.el
@@ -1305,7 +1305,7 @@ SCHEDULED: %^t
   (setq-default
    magit-process-popup-time 10
    magit-diff-refine-hunk t
-   magit-completing-read-function 'magit-ido-completing-read)
+   magit-completing-read-function 'magit-builtin-completing-read)
 
   ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
   ;; quickly open magit on any one of your projects.
