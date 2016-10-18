e5420695d2713d56f5bc032d8622a7d831deb380
Will Cohen <willcohen@users.noreply.github.com>
2016-10-18 07:26:56 -0400

    Fix markdown-mode, switch to kramdown

--- wcohen.el
+++ wcohen.el
@@ -1161,13 +1161,13 @@ With arg N, insert N newlines."
 
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 
-(require-package 'markdown-mode)
-
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "kramdown"))
 
 (require-package 'yaml-mode)
 
